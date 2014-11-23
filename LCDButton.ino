31d5a02dfe96fa325471c1f2776ab036957a2376
Le Guyadec <clement.leguyadec@free.fr>
2014-11-23 18:29:43 +0100

    nettoyage des variables non utilisées

--- LCDButton.ino
+++ LCDButton.ino
@@ -13,9 +13,9 @@ int read_LCD_buttons(){               // read the buttons
 
     // For V1.1 us this threshold
     if (adc_key_in < 50)   return btnRIGHT;  
-    if (adc_key_in < 250)  return btnUP; 
-    if (adc_key_in < 450)  return btnDOWN; 
-    if (adc_key_in < 650)  return btnLEFT; 
+    //if (adc_key_in < 250)  return btnUP; 
+    //if (adc_key_in < 450)  return btnDOWN; 
+    //if (adc_key_in < 650)  return btnLEFT; 
     if (adc_key_in < 850)  return btnSELECT;  
 
     return btnNONE;                // when all others fail, return this.
